54b0e47ee5c3e000844405c99e067b6ae1dbd2bb
rojandahal <maxpanye60@gmail.com>
2023-04-10 15:30:53 +0545

    Header design updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Button from "../Buttons/Button";
 import Menu from "../Menu/Menu";
+import "./header.css";
 
 export default function Header() {
   return (
     <>
-      <Menu />
-      <Button text={"Contact Me"}></Button>
+      <ul>
+        <Menu />
+        <Button background="#d465ef" text={"Contact Me"}></Button>
+      </ul>
     </>
   );
 }
