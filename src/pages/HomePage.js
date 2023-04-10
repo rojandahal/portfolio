2c89da1216149f1bec70148fc8d3828dfbe2b20f
rojandahal <maxpanye60@gmail.com>
2023-04-10 17:19:48 +0545

    Home Page Updated

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,10 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Card from "../components/Cards/Card";
 import "./HomePage.css";
+import ProjectCard from "../components/Cards/ProjectCard";
+import mobileImage from "../assets/mobile.jpg";
+import laptopImage from "../assets/laptopimage.jpg";
+import backgroundImage from "../assets/background.jpg";
 
 export default function HomePage() {
   return (
@@ -22,6 +26,21 @@ export default function HomePage() {
             description="Hello this is the descriptio of first card"
           ></Card>
         </div>
+      </div>
+
+      <div className="project-container">
+        <ProjectCard
+          title="First Project"
+          cardImage={mobileImage}
+        ></ProjectCard>
+        <ProjectCard
+          title="Second Project"
+          cardImage={laptopImage}
+        ></ProjectCard>
+        <ProjectCard
+          title="Third Project"
+          cardImage={backgroundImage}
+        ></ProjectCard>
       </div>
     </div>
   );
