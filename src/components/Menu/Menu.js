9e0ffdf503d860087b46c29efe273c55de2891b5
rojandahal <maxpanye60@gmail.com>
2023-04-10 14:42:43 +0545

    Header Menu added

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Blogs</li>
+      </ul>
+    </>
+  );
+}
