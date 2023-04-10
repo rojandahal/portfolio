578446ba391187bc9ac9c664f646b35df38c1438
rojandahal <maxpanye60@gmail.com>
2023-04-10 16:30:50 +0545

    Home Page Updated

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Card from "../components/Cards/Card";
+import "./HomePage.css";
 
 export default function HomePage() {
   return (
     <div>
       <Header></Header>
+      <div className="cards-container">
+        <div className="cards-list">
+          <Card
+            title="First One"
+            description="Hello this is the descriptio of first card"
+          ></Card>
+          <Card
+            title="First One"
+            description="Hello this is the descriptio of first card"
+          ></Card>
+          <Card
+            title="First One"
+            description="Hello this is the descriptio of first card"
+          ></Card>
+        </div>
+      </div>
     </div>
   );
 }
