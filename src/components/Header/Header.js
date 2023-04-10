6fbd2c20f764c3f41f3730992894d789de2c9037
rojandahal <maxpanye60@gmail.com>
2023-04-10 14:42:03 +0545

    Header file added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../Buttons/Button";
+import Menu from "../Menu/Menu";
+
+export default function Header() {
+  return (
+    <>
+      <Menu />
+      <Button text={"Contact Me"}></Button>
+    </>
+  );
+}
