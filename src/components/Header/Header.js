31007458ab60b2f9b0c4f5ac4cc27926d74e5b85
rojandahal <maxpanye60@gmail.com>
2023-04-11 13:12:46 +0545

    Page Header Updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,27 @@
-import React from "react";
-import Button from "../Buttons/Button";
+import React, { useEffect, useRef } from "react";
 import Menu from "../Menu/Menu";
 import "./header.css";
+import Typed from "typed.js";
 
 export default function Header() {
+  const el = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["<i>I am</i> a programmer.", "Developer", "Designer", "I am Rojan Dahal"],
+      typeSpeed: 50,
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
   return (
     <>
       <div className="header-container">
         <Menu />
-        <Button background="#d465ef" text={"Contact Me"}></Button>
+        <div className="type-animation" ref={el}></div>
       </div>
     </>
   );
