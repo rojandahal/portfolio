aad4852fd590f4ce7d12c68a63e25e8ee2f094cc
rojandahal <maxpanye60@gmail.com>
2023-04-17 11:41:08 +0545

    Responsive Projects

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -11,19 +11,29 @@ export default function AboutMe() {
   return (
     <>
       <div className="project-container">
-        <ProjectCard
-          title="First Project"
-          cardImage={mobileImage}
-        ></ProjectCard>
-        <ProjectCard
-          title="Second Project"
-          cardImage={laptopImage}
-        ></ProjectCard>
-        <ProjectCard
-          title="Third Project"
-          cardImage={backgroundImage}
-        ></ProjectCard>
+        <div className="project-wrapper">
+          <div className="project-text">
+            <span>Projects</span>
+            <h3>Some of my awesome stuff</h3>
+          </div>
+        </div>
+
+        <div className="project-cards-container">
+          <ProjectCard
+            title="First Project"
+            cardImage={mobileImage}
+          ></ProjectCard>
+          <ProjectCard
+            title="Second Project"
+            cardImage={laptopImage}
+          ></ProjectCard>
+          <ProjectCard
+            title="Third Project"
+            cardImage={backgroundImage}
+          ></ProjectCard>
+        </div>
       </div>
+
       <div className="about-me">
         <div className="container-about-me">
           <img src={me} alt="" srcset="" />
