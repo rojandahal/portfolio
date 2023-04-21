7a6f70056078b6eecda484e4471917a4513615f9
rojandahal <maxpanye60@gmail.com>
2023-04-21 14:07:29 +0545

    About me section improved

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -10,26 +10,31 @@ export default function AboutMe() {
       <Projects />
 
       <div className="about-me">
-        <div className="container-about-me">
+        <div className="image-about-me">
           <img src={me} alt="" />
-          <div className="about-me-description">
-            <h3>Title</h3>
-            <span>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </span>
-          </div>
+        </div>
+        <div className="about-me-description">
+          <h3>Rojan Dahal</h3>
+          <span>
+            As a computer engineering student, I have developed a strong skill
+            set in full-stack development using the MERN stack and Java. With
+            proficiency in MongoDB, Express, React, and Node.js, I am able to
+            create dynamic and interactive web applications. Additionally, I
+            have experience in Android app development, allowing me to design
+            and build applications for mobile devices. Throughout my studies, I
+            have acquired a solid understanding of programming languages,
+            software development, and computer hardware. This has enabled me to
+            tackle complex projects with confidence and precision. With a
+            passion for technology and a desire to continuously learn and
+            improve, I am committed to staying up-to-date with the latest
+            advancements in the field of computer engineering.
+          </span>
+        </div>
+        <div className="background-text">
+          <h1>About me</h1>
         </div>
       </div>
-      
+
       <Skills />
     </>
   );
